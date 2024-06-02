980439b8c1f00ae5242cbc9f30ca7b1e7f807b66
Bob <151000427+WilkBob@users.noreply.github.com>
2024-06-02 15:28:50 -0700

    Update Firebase configuration

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -7,14 +7,15 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyANf_ecqaIMHeCfElJgAgorGyIVgPByUf0",
+  authDomain: "e-commerce-4f380.firebaseapp.com",
+  databaseURL: "https://e-commerce-4f380-default-rtdb.firebaseio.com",
+  projectId: "e-commerce-4f380",
+  storageBucket: "e-commerce-4f380.appspot.com",
+  messagingSenderId: "989951804284",
+  appId: "1:989951804284:web:2b090c696b5e408bd0daf9",
+  measurementId: "G-54XP8ZZT1C"
 };
+
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
